ea40d1b53b333be5228d1967fb1dd9f7c8892365
IAmSSH <saadhasan620@gmail.com>
2018-09-07 22:42:36 +0530

    change Book Model to include Comments array

--- models/book.js
+++ models/book.js
@@ -3,7 +3,13 @@ var mongoose = require("mongoose");
 var bookSchema = new mongoose.Schema({
     name: String,
     image: String,
-    review: String
+    review: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 });
 
 module.exports = mongoose.model("Book", bookSchema);
