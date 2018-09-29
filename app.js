499b5101edc66090727b0d1122a283679548e067
IAmSSH <saadhasan620@gmail.com>
2018-09-29 19:33:59 +0530

    added login functionality but not tested

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-var Book            = require("/Programming/web-projects/BookReviewSite/models/book"),
-    methodOverride  = require("method-override"),
-    bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose"),
-    express         = require("express"),
-    request         = require("request"),
-    app             = express(),
-    commentRoutes   = require("./routes/comments");
-    bookRoutes      = require("./routes/books");
+var Book                    = require("/Programming/web-projects/BookReviewSite/models/book"),
+    User                    = require("./models/User"),
+    passportLocalMongoose   = require('passport-local-mongoose'),
+    commentRoutes           = require("./routes/comments"),
+    methodOverride          = require("method-override"),
+    indexRoutes             = require('./routes/index'),
+    LocalStrategy           = require('passport-local'),
+    bookRoutes              = require("./routes/books"),
+    bodyParser              = require("body-parser"),
+    mongoose                = require("mongoose"),
+    passport                = require('passport'),
+    express                 = require("express"),
+    request                 = require("request"),
+    app                     = express();
 
-    // SETUP CODE
-    app.set("view engine", "ejs");
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(express.static(__dirname + '/public'));
-    mongoose.connect("mongodb://localhost/book_review_app");
-    app.use(methodOverride('_method'));
-    app.use(commentRoutes);
-    app.use(bookRoutes);
+// SETUP CODE
+mongoose.connect("mongodb://localhost/book_review_app");
+app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
+// Passport Setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-    // seedDB();
-    // var books = [
-    //     {name: "Book1", image: "https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, assumenda."},
-    //     {name: "Book2", image: "https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, assumenda."},
-    //     {name: "Book3", image: "https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, assumenda."}
-    // ];
+app.use(indexRoutes);
+app.use(commentRoutes);
+app.use(bookRoutes);
 
 app.listen(8080, function() {
     console.log("Magic happens at 8080...");
